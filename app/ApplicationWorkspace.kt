ebb69cc4fb1ef18bb6a4df7dd9b3fd7835173f88
MotCaiBanhBao <MotCaiBanhBao@github.com>
2020-10-19 23:13:37 +0700

    Connect to database

--- app/ApplicationWorkspace.kt
+++ app/ApplicationWorkspace.kt
@@ -1,12 +1,18 @@
 package luongvany.k12tt.app
 
 import javafx.scene.control.TabPane
+import luongvany.k12tt.util.createTables
+import luongvany.k12tt.util.enableConsoleLogger
+import org.jetbrains.exposed.sql.Database
 import tornadofx.*
 
 class ApplicationWorkspace : Workspace("Application", NavigationMode.Tabs) {
 
     init {
         //Db
+        enableConsoleLogger()
+        Database.connect("jdbc:sqlite:./app-test.db", "org.sqlite.JDBC")
+        createTables()
 
         //Controller
 

8bcff8a07bd621d4a494c49c57d4653af70e6550
MotCaiBanhBao <MotCaiBanhBao@github.com>
2020-10-20 19:33:50 +0700

    Add a fun to convert entry to model

--- util/utilities.kt
+++ util/utilities.kt
@@ -1,7 +1,10 @@
 package luongvany.k12tt.util
 
+import luongvany.k12tt.model.StaffEntry
+import luongvany.k12tt.model.StaffEntryModel
 import org.joda.time.DateTime
 import java.time.LocalDate
+import luongvany.k12tt.model.Sex
 
 fun DateTime.toJavaLocalDate(): LocalDate{
 
@@ -11,4 +14,18 @@ fun DateTime.toJavaLocalDate(): LocalDate{
 
 fun LocalDate.toDate(default: DateTime = DateTime(1900, 1, 1, 0, 0, 0)): DateTime{
     return DateTime(this.year, this.monthValue, this.dayOfMonth, 0, 0)
+}
+
+fun StaffEntryModel.toStaffEntry(): StaffEntry{
+    return StaffEntry(this.id.value, this.name.value, this.homeTown.value, this.sex.value, this.birthDay.value, this.departmentId.value,
+            this.salaryId.value, this.img.value)
+}
+
+fun String.convertToSex(): Sex{
+    return if(this.toLowerCase().contains("nam"))
+        Sex.NAM
+    else if(this.toLowerCase().contains("nu"))
+        Sex.NU
+    else
+        Sex.GIOITINHTHUBA
 }

006070d1e758444ce56754346dc0238a74b569a0
MotCaiBanhBao <MotCaiBanhBao@github.com>
2020-10-20 19:30:16 +0700

    Restructure file add all some fun

--- view/staffview/addstaffview/AddStaff.kt
+++ view/staffview/addstaffview/AddStaff.kt
@@ -1,46 +1,94 @@
 package luongvany.k12tt.view.staffview.addstaffview
 
-import luongvany.k12tt.controller.StaffViewController
+import com.sun.javafx.css.StyleCache
+import javafx.scene.input.KeyCode
+import luongvany.k12tt.controller.ItemController
 import luongvany.k12tt.model.Sex
+import luongvany.k12tt.model.StaffEntryModel
+import luongvany.k12tt.util.toStaffEntry
+import luongvany.k12tt.view.staffview.stafftableview.CenterView
 import tornadofx.*
+import java.time.LocalDate
 
 class AddStaff: View("Thêm nhân viên"){
-    private val mainController: StaffViewController by inject()
+    val model = StaffEntryModel()
+    private val itemController: ItemController by inject()
 
     override val root = form {
-            fieldset {
-                field("Mã nhân viên") {
-                    textfield(mainController.idStaff){}
-                }
-                field("Tên nhân viên") {
-                    textfield(mainController.name){}
-                }
-                field("Quê quán") {
-                    textfield(mainController.homeTown){}
-                }
-                field("Giới tính") {
-                    combobox<Sex>(mainController.sex){
-                        items = mainController.gioiTinh
-                    }
-                }
-                field("Ngày sinh") {
-                    datepicker(mainController.birthDay){}
-                }
-                field("Mã phòng ban") {
-                    combobox<String>(mainController.departmentId){}
-                }
-                field("Mã Lương") {
-                    textfield(mainController.salaryId){}
-                }
-                field("Hình Ảnh") {
-                    textfield(mainController.img){}
-                }
-                button("Save"){
-                    shortcut("enter")
-                    action{
-                        mainController.saveStaff()
+        fieldset {
+            field("Mã nhân viên") {
+                textfield(model.id){
+                    this.required()
+                    validator {
+                        when{
+                            it.isNullOrEmpty() || it == " " ->error("Không được để trống")
+                            it == "0" ->error("Số phải khác không")
+                            itemController.items.any { itemId -> itemId.id.value == it.toInt() } -> error("Id đã trùng")
+                            !it.isLong() -> error("Mã nhân viên cần là số")
+                            else -> null
+                        }
                     }
                 }
             }
+            field("Tên nhân viên") {
+                textfield(model.name){
+                    this.required()
+                    validator {
+                        when{
+                            it.isNullOrEmpty() || it == " " ->error("Không được để trống")
+                            it.length>30 -> error("Quá dài")
+                            else -> null
+                        }
+                    }
+                }
+
+            }
+            field("Quê quán") {
+                textfield(model.homeTown){
+                    this.required()
+                    validator {
+                        when{
+                            it.isNullOrEmpty() || it == " " ->error("Không được để trống")
+                            it.length>30 -> error("Quá dài")
+                            else -> null
+                        }
+                    }
+                }
+            }
+            field("Giới tính") {
+                combobox<Sex>(model.sex){
+                    items = itemController.gioiTinh
+                }
+            }
+            field("Ngày sinh") {
+                datepicker(model.birthDay){
+                    this.required()
+                    validator {
+                        when{
+                           it?.dayOfYear.toString().isEmpty() -> error("Tuổi không hợp lệ")
+                            else -> null
+                        }
+                    }
+                }
+            }
+            field("Mã phòng ban") {
+                combobox<Number>(model.departmentId){}
+            }
+            field("Mã Lương") {
+                textfield(model.salaryId){}
+            }
+            field("Hình Ảnh") {
+                textfield(model.img){}
+            }
+            button("Save"){
+                enableWhen(model.valid)
+                action{
+                    model.commit{
+                        itemController.add(model.toStaffEntry())
+                        model.rollback()
+                    }
+                }
+            }
+        }
     }
 }

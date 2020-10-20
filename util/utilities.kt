cd4ac0e560248e3b52b22b33b8680ab35e23da88
MotCaiBanhBao <MotCaiBanhBao@github.com>
2020-10-20 11:31:49 +0700

    Add some utilities to work with date

--- util/utilities.kt
+++ util/utilities.kt
@@ -6,4 +6,9 @@ import java.time.LocalDate
 fun DateTime.toJavaLocalDate(): LocalDate{
 
     return LocalDate.of(this.year, this.monthOfYear, this.dayOfMonth)
+
+}
+
+fun LocalDate.toDate(default: DateTime = DateTime(1900, 1, 1, 0, 0, 0)): DateTime{
+    return DateTime(this.year, this.monthValue, this.dayOfMonth, 0, 0)
 }

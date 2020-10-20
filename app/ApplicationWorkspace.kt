f4ba5b9b3f75f85225da542f9c2da74aa3b5bad9
MotCaiBanhBao <MotCaiBanhBao@github.com>
2020-10-20 11:26:16 +0700

    Add some controller and setup connect to database(Sqlite)

--- app/ApplicationWorkspace.kt
+++ app/ApplicationWorkspace.kt
@@ -1,10 +1,19 @@
 package luongvany.k12tt.app
 
 import javafx.scene.control.TabPane
+import javafx.scene.image.Image
+import luongvany.k12tt.controller.ItemController
+import luongvany.k12tt.model.Sex
+import luongvany.k12tt.model.Staff
+import luongvany.k12tt.model.StaffEntryTbl
 import luongvany.k12tt.util.createTables
 import luongvany.k12tt.util.enableConsoleLogger
+import luongvany.k12tt.util.execute
+import luongvany.k12tt.util.toDate
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.insert
 import tornadofx.*
+import java.time.LocalDate
 
 class ApplicationWorkspace : Workspace("Application", NavigationMode.Tabs) {
 
@@ -14,6 +23,7 @@ class ApplicationWorkspace : Workspace("Application", NavigationMode.Tabs) {
         Database.connect("jdbc:sqlite:./app-test.db", "org.sqlite.JDBC")
         createTables()
 
+        ItemController()
         //Controller
 
         //dock

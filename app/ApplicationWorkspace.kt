48e0742f6294ab603491a1cc5589e8918acef15d
MotCaiBanhBao <MotCaiBanhBao@github.com>
2020-10-20 19:30:38 +0700

    Edit some fun

--- app/ApplicationWorkspace.kt
+++ app/ApplicationWorkspace.kt
@@ -1,19 +1,12 @@
 package luongvany.k12tt.app
 
 import javafx.scene.control.TabPane
-import javafx.scene.image.Image
 import luongvany.k12tt.controller.ItemController
-import luongvany.k12tt.model.Sex
-import luongvany.k12tt.model.Staff
-import luongvany.k12tt.model.StaffEntryTbl
 import luongvany.k12tt.util.createTables
 import luongvany.k12tt.util.enableConsoleLogger
-import luongvany.k12tt.util.execute
-import luongvany.k12tt.util.toDate
+import luongvany.k12tt.view.staffview.stafftableview.StaffView
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.insert
 import tornadofx.*
-import java.time.LocalDate
 
 class ApplicationWorkspace : Workspace("Application", NavigationMode.Tabs) {
 
@@ -26,7 +19,8 @@ class ApplicationWorkspace : Workspace("Application", NavigationMode.Tabs) {
         ItemController()
         //Controller
 
-        //dock
+        //dock our view
+        dock<StaffView>()
 
         tabContainer.tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
     }

2000c0d9ccf9d433721bb3790366fde8ae38c780
MotCaiBanhBao <MotCaiBanhBao@github.com>
2020-10-29 16:33:01 +0700

    Add user singletons

---
+++ model/User.kt
@@ -0,0 +1,9 @@
+package luongvany.k12tt.model
+
+import javafx.beans.property.SimpleStringProperty
+import tornadofx.*
+
+object User{
+    var userName = SimpleStringProperty()
+    var password = SimpleStringProperty()
+}

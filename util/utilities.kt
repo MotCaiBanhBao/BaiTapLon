944ccb3a05d6ae6da6a9ba204cfe56790233dfd6
MotCaiBanhBao <MotCaiBanhBao@github.com>
2020-10-30 07:09:35 +0700

    Add some utility

--- util/utilities.kt
+++ util/utilities.kt
@@ -5,6 +5,8 @@ import luongvany.k12tt.model.datamodel.StaffEntryModel
 import org.joda.time.DateTime
 import java.time.LocalDate
 import luongvany.k12tt.model.Sex
+import luongvany.k12tt.model.datamodel.DepartmentEntry
+import luongvany.k12tt.model.datamodel.DepartmentEntryModel
 
 fun DateTime.toJavaLocalDate(): LocalDate{
 
@@ -16,10 +18,9 @@ fun LocalDate.toDate(default: DateTime = DateTime(1900, 1, 1, 0, 0, 0)): DateTim
     return DateTime(this.year, this.monthValue, this.dayOfMonth, 0, 0)
 }
 
-fun StaffEntryModel.toStaffEntry(): StaffEntry {
-    return StaffEntry(this.id.value, this.name.value, this.homeTown.value, this.sex.value, this.birthDay.value, this.departmentId.value,
-            this.salaryId.value, this.img.value)
-}
+fun StaffEntryModel.toStaffEntry() = StaffEntry(this.id.value, this.name.value, this.homeTown.value,
+        this.sex.value, this.birthDay.value, this.departmentId.value,
+        this.salaryId.value, this.img.value)
 
 fun String.convertToSex(): Sex{
     return if(this.toLowerCase().contains("nam"))
@@ -29,3 +30,13 @@ fun String.convertToSex(): Sex{
     else
         Sex.GIOITINHTHUBA
 }
+
+fun StaffEntryModel.coppy() = StaffEntryModel().apply {
+    item = this@coppy.toStaffEntry()
+}
+fun StaffEntry.toStaffEntryModel(): StaffEntryModel{
+    return StaffEntryModel().apply {
+        item = this@toStaffEntryModel
+    }
+}
+fun DepartmentEntryModel.toDepartmentEntry() = DepartmentEntry(this.id.value, this.departmentName.value, this.managerId.value ,this.directorateId.value)
